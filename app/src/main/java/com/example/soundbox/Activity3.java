a5c9dfeb1eaed25a8ea2205ad79c3f5d87e779ae
cherbuyl8 <leo.cherbuy@reseau.eseo.fr>
2021-01-13 21:48:39 +0100

    Remise aux normes (définitif)

--- app/src/main/java/com/example/soundbox/Activity3.java
+++ app/src/main/java/com/example/soundbox/Activity3.java
@@ -159,6 +159,5 @@ public class Activity3 extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         startActivity(intent);
-        //ceci est un test de commit1
     }
 }
